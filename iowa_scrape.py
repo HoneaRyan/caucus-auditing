f969f3a32b40d6322fa9c87a8e2df41190d951b4
jrmcgaha-dev <j.rmcgaha2014@gmail.com>
2020-02-07 20:43:30 -0500

    Clean up commented code

--- iowa_scrape.py
+++ iowa_scrape.py
@@ -17,11 +17,6 @@ def extract_cols(parent, header_row):
 if __name__ == "__main__":
     data = requests.get(URL).text
     soup = BeautifulSoup(data, 'lxml')
-    # header = soup.find("ul", {"class": "thead"})
-    # heads = header.find_all("li")
-    # subheader = soup.find("ul", {"class": "sub-head"})
-    # subheads = subheader.find_all("li")
-    # headers = ["county", "precinct", "candidate", "1stalign", "2ndalign", "sde"]
     precinct_table = soup.find('div', {'class': 'precinct-table'})
     # precinct_table limits our finds to only the contents we actually want
     source_heads = tuple(
@@ -43,28 +38,6 @@ if __name__ == "__main__":
     # Configures our headers to be of form
     # [Upper Header] or [Upper Header].[Lower Header]
     # for those columns that have sub-sections
-    # df = pd.DataFrame(columns=headers)
-    # for divs in soup.find_all("div", {"class": "precinct-rows"}):
-    #     county_nm = divs.find_all("div", {"class": "wrap"})[0]
-    #     precincts = divs.find_all("div", {"class": "precinct-data"})
-    #     vals = precincts[0].find_all('li')
-    #     rows = int(len(vals) / 43)
-    #     for i in range(0, rows - 1):
-    #         for j in range(0, 14):
-    #             precinct = vals[i * 43]
-    #             candidate = heads[2 + j * 3]
-    #             align_1 = vals[i * 43 + j * 3 + 1]
-    #             align_2 = vals[i * 43 + j * 3 + 2]
-    #             sde = vals[i * 43 + j * 3 + 3]
-    #             df = df.append({
-    #                 'county': county_nm.string,
-    #                 'precinct': precinct.string,
-    #                 'candidate': candidate.string,
-    #                 '1stalign': align_1.string,
-    #                 '2ndalign': align_2.string,
-    #                 'sde': sde.string
-    #             }, ignore_index=True)
-    # df.to_csv('iowa_caucuses.csv')
     counties = precinct_table.find_all('div', {'class': 'precinct-rows'})
     # generates a list of all of the discovered county objects
     storage_list = list()
